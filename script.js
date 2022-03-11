ba50fbc0346b2658c25f36072076f7c9668d6f52
Jacob Sanders <sanders.jms@gmail.com>
2022-03-11 13:34:45 -0700

    set querySelectors and an Event listener to change display

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const display = document.querySelector('.display');
+const numButton = document.querySelectorAll('.num');
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -7,3 +10,10 @@ function operate(operator, prevNum, currNum) {
     let result = operator(prevNum, currNum)
     return result;
 }
+
+function changeDisplay(e) {
+    display.textContent = this.textContent;
+}
+
+
+numButton.forEach(button => button.addEventListener('click', changeDisplay));
